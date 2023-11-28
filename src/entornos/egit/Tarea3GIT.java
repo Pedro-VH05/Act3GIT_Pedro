e41f2428debd0e6ed72395cf82f081190cd59f81
Pedro-VH05 <a_pedro.vigara.haro@iespablopicasso.es>
2023-11-28 09:47:17 +0100

    Creo el generador de números aleatorios

--- src/entornos/egit/Tarea3GIT.java
+++ src/entornos/egit/Tarea3GIT.java
@@ -1,10 +1,18 @@
 package entornos.egit;
-
+import java.util.Random;
 public class Tarea3GIT {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    
+    System.out.println("NÚMEROS ALEATORIOS TAREA EGIT");
+    System.out.println("-----------------------------");
+    
+    Random generadorNumeros = new Random();
+    for (int i = 0; i <= 20; i++) {
+      double numeroAleatorio = (generadorNumeros.nextDouble() * 10) + 1;
+      System.out.println(numeroAleatorio);
+    }
+    
   }
 
 }
