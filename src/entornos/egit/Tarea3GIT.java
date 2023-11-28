b74df2c00820362c234a39a6e4bfeb74c9bce53e
Pedro-VH05 <a_pedro.vigara.haro@iespablopicasso.es>
2023-11-28 10:35:27 +0100

    Añado intervalos y suma de 7 en 7

--- src/entornos/egit/Tarea3GIT.java
+++ src/entornos/egit/Tarea3GIT.java
@@ -1,15 +1,20 @@
 package entornos.egit;
 import java.util.Random;
+import java.util.Scanner;
 public class Tarea3GIT {
-
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     
     System.out.println("NÚMEROS ALEATORIOS TAREA EGIT");
     System.out.println("-----------------------------");
+    System.out.println("Escribe el intervalo inicial de números: ");
+    int primerIntervalo = Integer.parseInt(sc.nextLine());
+    System.out.println("Escribe el intervalo final de números: ");
+    int segundoIntervalo = Integer.parseInt(sc.nextLine());
     
     Random generadorNumeros = new Random();
-    for (int i = 0; i <= 20; i++) {
-      double numeroAleatorio = (generadorNumeros.nextDouble() * 10) + 1;
+    for (int i = 0; i <= segundoIntervalo; i+= 7) {
+      double numeroAleatorio = (generadorNumeros.nextDouble() * (segundoIntervalo - primerIntervalo)) + primerIntervalo;
       System.out.println(numeroAleatorio);
     }
     
