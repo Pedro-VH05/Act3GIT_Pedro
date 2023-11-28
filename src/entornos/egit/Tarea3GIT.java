39d8f6ea9d04a49679a8923499e0446b05e8b2a2
Pedro-VH05 <a_pedro.vigara.haro@iespablopicasso.es>
2023-11-28 12:22:51 +0100

    Tercer número intervalo

--- src/entornos/egit/Tarea3GIT.java
+++ src/entornos/egit/Tarea3GIT.java
@@ -11,11 +11,19 @@ public class Tarea3GIT {
     int primerIntervalo = Integer.parseInt(sc.nextLine());
     System.out.println("Escribe el intervalo final de números: ");
     int segundoIntervalo = Integer.parseInt(sc.nextLine());
+    System.out.println("Escribe un número que esté dentro de los intervalos de números: ");
+    int numeroDentroIntervalo = Integer.parseInt(sc.nextLine());
     
     Random generadorNumeros = new Random();
     for (int i = 0; i <= segundoIntervalo; i+= 7) {
       double numeroAleatorio = (generadorNumeros.nextDouble() * (segundoIntervalo - primerIntervalo)) + primerIntervalo;
       System.out.println(numeroAleatorio);
+    }
+    
+    if ((primerIntervalo <= numeroDentroIntervalo) && (numeroDentroIntervalo <= segundoIntervalo)) {
+      System.out.println("El número " + numeroDentroIntervalo + " está dentro del intervalo");
+    } else {
+      System.out.println("El número " + numeroDentroIntervalo + " no está dentro del intervalo");
     }
     
   }
